01d5da4bf2601651837e60a7d393ebc250df2798
mdaasim <work.aasim@gmail.com>
2021-12-30 16:46:07 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -118,39 +118,32 @@
     AOS.refresh();
   });
 
-  /*-----------------menudropdown------------------*/
 
-  /*document.addEventListener("DOMContentLoaded", function(){
-		if (window.innerWidth > 992) {
-		
-			document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
-		
-				everyitem.addEventListener('mouseover', function(e){
-		
-					let el_link = this.querySelector('a[data-bs-toggle]');
-		
-					if(el_link != null){
-						let nextEl = el_link.nextElementSibling;
-						el_link.classList.add('show');
-						nextEl.classList.add('show');
-					}
-		
-				});
-				everyitem.addEventListener('mouseleave', function(e){
-					let el_link = this.querySelector('a[data-bs-toggle]');
-		
-					if(el_link != null){
-						let nextEl = el_link.nextElementSibling;
-						el_link.classList.remove('show');
-						nextEl.classList.remove('show');
-					}
-		
-		
-				})
-			});
-		
-		}
-	});*/
+  jQuery(document).ready(function ($) {
+    var feedbackSlider = $(".feedback-slider");
+    feedbackSlider.owlCarousel({
+      items: 1,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      loop: true,
+      mouseDrag: true,
+      touchDrag: true,
+      navText: [
+        "<i class='fa fa-long-arrow-left'></i>",
+        "<i class='fa fa-long-arrow-right'></i>"
+      ],
+      responsive: {
+        // breakpoint from 767 up
+        767: {
+          nav: true,
+          dots: false
+        }
+      }
+    });
+  }); //end ready
+  
+
 })(jQuery, window);
 
 /*--------------Menu---------------------*/
